25eff0e772919e7a0a32ed9dc650b2d68ae1b12c
Bevis_Liu <bevis.liu@sgs.com>
2024-12-03 16:26:34 +0800

    add baidu tongji

--- assets/hb/modules/custom/js/index.ts
+++ assets/hb/modules/custom/js/index.ts
@@ -1,1 +1,8 @@
 // This script will be compiled into the JS bundle automatically.
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?074bd235e4925e70a30692acaf532bb8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
